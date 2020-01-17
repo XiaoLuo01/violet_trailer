43ef299632113514b31bd721a4989b8b49179bc7
XiaoLuo01 <1428620591@qq.com>
2020-01-17 20:03:09 +0800

    add HTML template

--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
 const Koa = require('koa')
 const app = new Koa()
+const { htmlTpl } = require('./template')
 
 app.use(async (ctx, next) => {
-  ctx.body = '电影首页'
+  ctx.type = 'text/html; charset=utf-8'
+  ctx.body = htmlTpl
 })
 
 app.listen(2233)
