ec6cb518e690e912ffa50b07a9b0702e9319cfc9
XiaoLuo01 <1428620591@qq.com>
2020-01-30 23:19:45 +0800

    add koa-router

--- server/index.js
+++ server/index.js
@@ -4,14 +4,18 @@ const app = new Koa()
 const views = require('koa-views')
 const { resolve } = require('path')
 const { connect, initSchemas } = require('./database/init')
+const router = require('./routes/index')
 
 ;(async () => {
   await connect()
 
   initSchemas()
 
-  require('./tasks/api')
+  // require('./tasks/api')
 })()
+
+app.use(router.routes())
+  .use(router.allowedMethods())
 
 // 静态资源文件加载
 app.use(views(resolve(__dirname, './views'), {
